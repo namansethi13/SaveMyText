d0cd4be9bb5987a020adabce5ba9ed36c3443d9a
stinger13 <93597591+stinger13@users.noreply.github.com>
2022-09-09 11:19:11 +0530

    fixed import url to re_path

--- core/urls.py
+++ core/urls.py
@@ -14,18 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include, re_path
 from sitehandler import views as sitehandler_views
 import sitehandler
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('savetext', sitehandler_views.savetext, name = "savetext"),
     path('',include("home.urls")),
     path('<slug:site_url>/',sitehandler_views.open_page),
