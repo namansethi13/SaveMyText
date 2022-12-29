c1f9f9d46de461db8d58afcd8c01a8040db03ebc
stinger13 <93597591+stinger13@users.noreply.github.com>
2022-12-29 14:37:03 +0530

    changes in settings for vercel

--- core/settings.py
+++ core/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': 'rxjaopcl',
-#     'USER': 'rxjaopcl',
-#     'PASSWORD': 'vx9skW3jSQW3TWgsILlSJZ5ftK1LwVh2',
-#     # ↓ HOST instead of HOSTS
-#     'HOST': 'arjuna.db.elephantsql.com',
-#     'PORT': 5432
-#   }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'rxjaopcl',
+    'USER': 'rxjaopcl',
+    'PASSWORD': 'vx9skW3jSQW3TWgsILlSJZ5ftK1LwVh2',
+    # ↓ HOST instead of HOSTS
+    'HOST': 'arjuna.db.elephantsql.com',
+    'PORT': 5432
+  }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
